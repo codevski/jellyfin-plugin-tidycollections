3cad8f439aad2606a4e136ad599accb236281b36
codevski <1435321+codevski@users.noreply.github.com>
2023-12-26 16:56:33 +1100

    init

---
+++ Jellyfin.Plugin.TidyCollections/Plugin.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Jellyfin.Plugin.TidyCollections.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Jellyfin.Plugin.TidyCollections;
+
+/// <summary>
+/// The main plugin.
+/// </summary>
+public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+{
+  /// <summary>
+  /// Initializes a new instance of the <see cref="Plugin"/> class.
+  /// </summary>
+  /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
+  /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
+  public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+      : base(applicationPaths, xmlSerializer)
+  {
+    Instance = this;
+  }
+
+  /// <inheritdoc />
+  public override string Name => "TidyCollections";
+
+  /// <inheritdoc />
+  public override Guid Id => Guid.Parse("6d5bb3cf-65f3-4dfa-a1a7-4bfa5199f6f8");
+
+  /// <summary>
+  /// Gets the current plugin instance.
+  /// </summary>
+  public static Plugin? Instance { get; private set; }
+
+  /// <inheritdoc />
+  public IEnumerable<PluginPageInfo> GetPages()
+  {
+    return new[]
+    {
+            new PluginPageInfo
+            {
+                Name = this.Name,
+                EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configPage.html", GetType().Namespace)
+            }
+    };
+  }
+}
